62830066ba5e06950c9714f601cb5e0b1b372f4c
Lucas GT <lucas.g.theodooro@gmail.com>
2024-03-30 11:53:00 -0300

    Rotas funcionando agr

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,29 +1,27 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import Home from './components/pages/Home';
-import Contato from './components/pages/Contato';
-import NewProject from './components/pages/NewProject';
-import Empresa from './components/pages/Empresa';
-
-import Container from './layout/Container';
+import Home from './components/pages/Home/Pages';
+import NewProject from './components/pages/NewProject/Pages';
+import Empresa from './components/pages/Empresa/Pages';
+import Contato from './components/pages/Contatos/Pages';
 
 
 function App() {
   return (
+    
     <Router>
       <div>
+        <Link to='/Contato'></Link>
         <Link to="/">Home</Link>
-        <Link to="/Empresa">EmpresaContato</Link>
+        <Link to="/Empresa">Empresa</Link>
         <Link to="/Contato">Contato</Link>
         <Link to="/NewProject">Novo projeto</Link>
       </div>
 
       <Routes>
-        <Container>
           <Route exact path='/' element={<Home/>}></Route>
           <Route path='/Empresa' element={<Empresa/>}></Route>
           <Route path='/Contato' element={<Contato/>}></Route>
           <Route path='/NewProject' element={<NewProject/>}></Route>
-        </Container>
       </Routes>
       <p>Footer</p>
     </Router>
